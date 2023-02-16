f9c8dcc859cf09fa086af492f77838af7c831a3f
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-02-16 18:37:03 +0100

    post and get test

--- task/test/app.e2e-spec.ts
+++ task/test/app.e2e-spec.ts
@@ -3,9 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { CreateTaskDto } from './../src/task/dto/create-task.dto';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const prisma = new PrismaClient();
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,6 +16,22 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  beforeAll(async () => {
+    const deleteTasks = prisma.task.deleteMany();
+
+    await prisma.$transaction([deleteTasks]);
+
+    await prisma.$disconnect();
+  });
+
+  afterEach(async () => {
+    const deleteTasks = prisma.task.deleteMany();
+
+    await prisma.$transaction([deleteTasks]);
+
+    await prisma.$disconnect();
   });
 
   it('/task (POST)', () => {
@@ -30,6 +48,33 @@ describe('AppController (e2e)', () => {
           expect.objectContaining({
             title: '01',
             dueDate: dueDate.toISOString(),
+          }),
+        );
+      });
+  });
+
+  it('/task (GET)', () => {
+    return request(app.getHttpServer()).get('/task').expect(200).expect([]);
+  });
+
+  it('/task/:id (GET)', async () => {
+    let id: number;
+    await request(app.getHttpServer())
+      .post('/task')
+      .send({
+        title: '01',
+      } as CreateTaskDto)
+      .expect(201)
+      .then(({ body }) => (id = body.id));
+
+    return request(app.getHttpServer())
+      .get('/task/' + id)
+      .expect(200)
+      .expect(function (res) {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            title: '01',
+            id,
           }),
         );
       });
